843cb8a7b28f57e1610a1eabebfc968bb3fd5764
Row <axlrower@gmail.com>
2025-11-11 15:21:22 -0300

    Fix: Cambiar a HashRouter para evitar error 404 en Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import IniciarSesion from "./views/IniciarSesion.jsx";
 import Detalles from "./views/Detalles.jsx";
